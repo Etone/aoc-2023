896f4a4472ecfddc3c259b7b61c995a35fc39cd9
Etone <co.schapoehler@gmail.com>
2023-12-03 17:20:03 +0000

    Day 3: Part 2

--- 03/sln.go
+++ 03/sln.go
@@ -36,7 +36,7 @@ func readFileToGrid(path string) (grid grid) {
 }
 
 func isPartNumber(position position, grid grid) bool {
-	adjacent := getAdjacentCells(position, grid)
+	adjacent := getAdjacentCellValues(position, grid)
 	for _, cell := range adjacent {
 		if !unicode.IsDigit(cell) && cell != '.' {
 			return true
@@ -45,7 +45,7 @@ func isPartNumber(position position, grid grid) bool {
 	return false
 }
 
-func getAdjacentCells(position position, grid grid) (adjacent []cell) {
+func getAdjacentCellValues(position position, grid grid) (adjacent []cell) {
 	xTop := max(position.x-1, 0)
 	xBot := min(position.x+1, len(grid)-1)
 	yLeft := max(position.y-1, 0)
@@ -65,6 +65,38 @@ func getAdjacentCells(position position, grid grid) (adjacent []cell) {
 	return
 }
 
+func getAdjacentCellPositions(pos position, grid grid) (adjacent []position) {
+	xTop := max(pos.x-1, 0)
+	xBot := min(pos.x+1, len(grid)-1)
+	yLeft := max(pos.y-1, 0)
+	yRight := min(pos.y+1, len(grid[0])-1)
+
+	topLeft := position{x: xTop, y: yLeft, cell: grid[xTop][yLeft]}
+	top := position{x: xTop, y: pos.y, cell: grid[xTop][pos.y]}
+	topRight := position{x: xTop, y: yRight, cell: grid[xTop][yRight]}
+
+	left := position{x: pos.x, y: yLeft, cell: grid[pos.x][yLeft]}
+	right := position{x: pos.x, y: yRight, cell: grid[pos.x][yRight]}
+
+	botLeft := position{x: xBot, y: yLeft, cell: grid[xBot][yLeft]}
+	bot := position{x: xBot, y: pos.y, cell: grid[xBot][pos.y]}
+	botRight := position{x: xBot, y: yRight, cell: grid[xBot][yRight]}
+
+	adjacent = append(adjacent, topLeft, top, topRight, left, right, botLeft, bot, botRight)
+
+	return
+}
+
+func gear(pos position, grid grid) (bool, position) {
+	adjacent := getAdjacentCellPositions(pos, grid)
+	for _, p := range adjacent {
+		if p.cell == '*' {
+			return true, p
+		}
+	}
+	return false, pos
+}
+
 func part1() {
 	grid := readFileToGrid("input_01.txt")
 
@@ -78,13 +110,11 @@ func part1() {
 
 			if unicode.IsDigit(cell) {
 				currentNumber.WriteRune(cell)
-				println(currentNumber.String())
 				isTouching = isTouching || isPartNumber(pos, grid)
 			} else if currentNumber.Len() != 0 {
 				if isTouching {
 					partNumber, _ := strconv.Atoi(currentNumber.String())
 					sumOfPartNumbers += partNumber
-					println(partNumber, sumOfPartNumbers)
 				}
 				currentNumber.Reset()
 				isTouching = false
@@ -96,15 +126,42 @@ func part1() {
 }
 
 func part2() {
-	grid := readFileToGrid("example.txt")
+	grid := readFileToGrid("input_01.txt")
 
 	sumOfGearRatios := 0
 
+	gearsAndCells := map[position][]int{}
+
 	for row, cells := range grid {
 		var currentNumber strings.Builder
-		isTouching := false
+		isNextToGear := false
+		var gearNextTo position
+
 		for col, cell := range cells {
-			pos := position{cell: cell, x: row, y: col}
+
+			pos := position{x: row, y: col, cell: cell}
+
+			if unicode.IsDigit(cell) {
+				currentNumber.WriteRune(cell)
+				exists, gearPos := gear(pos, grid)
+				isNextToGear = isNextToGear || exists
+				if exists {
+					gearNextTo = gearPos
+				}
+			} else if currentNumber.Len() != 0 {
+				if isNextToGear {
+					current, _ := strconv.Atoi(currentNumber.String())
+					gearsAndCells[gearNextTo] = append(gearsAndCells[gearNextTo], current)
+				}
+				currentNumber.Reset()
+				isNextToGear = false
+			}
+		}
+
+	}
+	for _, numbers := range gearsAndCells {
+		if len(numbers) == 2 {
+			sumOfGearRatios += numbers[0] * numbers[1]
 		}
 	}
 
