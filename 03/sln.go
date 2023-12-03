d86f1775f86f0b998f3cafb5d59865ee49e991da
Etone <co.schapoehler@gmail.com>
2023-12-03 14:01:08 +0000

    Day 3: Part 1

--- 03/sln.go
+++ 03/sln.go
@@ -49,7 +49,7 @@ func getAdjacentCells(position position, grid grid) (adjacent []cell) {
 	xTop := max(position.x-1, 0)
 	xBot := min(position.x+1, len(grid)-1)
 	yLeft := max(position.y-1, 0)
-	yRight := min(position.y+1, len(grid)-1)
+	yRight := min(position.y+1, len(grid[0])-1)
 
 	topLeft := grid[xTop][yLeft]
 	top := grid[xTop][position.y]
@@ -66,7 +66,7 @@ func getAdjacentCells(position position, grid grid) (adjacent []cell) {
 }
 
 func part1() {
-	grid := readFileToGrid("example.txt")
+	grid := readFileToGrid("input_01.txt")
 
 	sumOfPartNumbers := 0
 
@@ -78,13 +78,13 @@ func part1() {
 
 			if unicode.IsDigit(cell) {
 				currentNumber.WriteRune(cell)
+				println(currentNumber.String())
 				isTouching = isTouching || isPartNumber(pos, grid)
-
 			} else if currentNumber.Len() != 0 {
 				if isTouching {
 					partNumber, _ := strconv.Atoi(currentNumber.String())
-					fmt.Println(partNumber)
 					sumOfPartNumbers += partNumber
+					println(partNumber, sumOfPartNumbers)
 				}
 				currentNumber.Reset()
 				isTouching = false
@@ -96,4 +96,17 @@ func part1() {
 }
 
 func part2() {
+	grid := readFileToGrid("example.txt")
+
+	sumOfGearRatios := 0
+
+	for row, cells := range grid {
+		var currentNumber strings.Builder
+		isTouching := false
+		for col, cell := range cells {
+			pos := position{cell: cell, x: row, y: col}
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", sumOfGearRatios)
 }
